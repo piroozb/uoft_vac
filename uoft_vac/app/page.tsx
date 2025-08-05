9794804f2590e53cbd899ce2d88ecb0801d4a4ec
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-08-04 21:30:06 -0700

    Added a white box in place for the banner. I think the navbar is pushing it down so I need it to be below it to fit exactly in the window.

--- uoft_vac/app/page.tsx
+++ uoft_vac/app/page.tsx
@@ -1,18 +1,10 @@
-import Image from "next/image";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="rounded-3xl bg-green-300 w-3xl h-100 p-10 text-blue-500">
-        <h1 className="p-2 px-4 bg-yellow-100 h-13 w-2xl rounded-4xl text-3xl">Our Goals:</h1>
-        <div className="flex flex-row justify-center gap-16 p-5">
-        <p className="p-2 bg-yellow-100 h-10 w-3xs rounded-4xl text-center">Create</p>
-        <p className="p-2 bg-yellow-100 h-10 w-3xs rounded-4xl text-center">Share</p>
-        <p className="p-2 bg-yellow-100 h-10 w-3xs rounded-4xl text-center">Connect</p>
-        </div>
-        </div>
-      </main>
-    </div>
+    <>
+      <Banner />
+      {/* Featured post and events will come after */}
+    </>
   );
 }
