9d051020f48a72b1615c0c42df2d8ce7f1bc1477
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-08-10 17:27:48 -0700

    Added the upcoming events section. Overall layout for the home page is finished.

--- uoft_vac/app/page.tsx
+++ uoft_vac/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "./components/home/Banner";
 import FeaturedPost from "./components/home/FeaturedPost";
+import UpcomingEvents from "./components/home/UpcomingEvents";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <FeaturedPost />
+      <UpcomingEvents />
     </>
   );
 }
