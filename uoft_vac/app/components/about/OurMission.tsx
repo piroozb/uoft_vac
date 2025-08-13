df7d2057e86ad254f2d6ee7b526ed639460429bc
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-08-13 13:36:44 -0700

    Edited the layout. Now the mission statement and values both fit on the screen. We can add decorations like Frodos on the left and right later.

--- uoft_vac/app/components/about/OurMission.tsx
+++ uoft_vac/app/components/about/OurMission.tsx
@@ -1,5 +1,8 @@
 import SectionTitle from "../SectionTitle";
 
+const valueClass =
+    "w-75 h-75 rounded-full flex items-center justify-center text-white text-lg font-bold";
+
 export default function OurMission() {
     return (
         <section className="flex flex-col items-center py-12 bg-[#b4f4b4]">
@@ -7,9 +10,9 @@ export default function OurMission() {
             <SectionTitle>Our Mission!</SectionTitle>
 
             {/* Paint splash */}
-            <div className="w-11/12 max-w-4xl bg-[rgb(53,228,78)] p-8 rounded-lg mt-6">
+            <div className="w-11/12 max-w-3xl bg-[rgb(53,228,78)] p-8 rounded-lg">
                 {/* Mission statement */}
-                <p className="text-[1.5rem] text-black leading-relaxed">
+                <p className="text-[1.5rem] text-black leading-relaxed text-center">
                     The <strong>University of Toronto Visual Art Club (UTVAC)</strong> aims to build an
                     <strong> encouraging, artistic community</strong> and a <strong>positive platform</strong> for students
                     interested in art to develop and express their passion through
@@ -18,22 +21,12 @@ export default function OurMission() {
                     to <strong>discover their creative potentials</strong>.
                 </p>
             </div>
+
             {/* Values */}
-            <div className="flex justify-center gap-20 mt-10">
-                {/* Create */}
-                <div className="w-100 h-100 rounded-full bg-red-500 flex items-center justify-center text-white text-lg font-bold">
-                    Create
-                </div>
-
-                {/* Share */}
-                <div className="w-100 h-100 rounded-full bg-yellow-400 flex items-center justify-center text-white text-lg font-bold">
-                    Share
-                </div>
-
-                {/* Connect */}
-                <div className="w-100 h-100 rounded-full bg-blue-500 flex items-center justify-center text-white text-lg font-bold">
-                    Connect
-                </div>
+            <div className="flex justify-center gap-30 mt-10">
+                <div className={`${valueClass} bg-red-500`}>Create</div>
+                <div className={`${valueClass} bg-yellow-400`}>Share</div>
+                <div className={`${valueClass} bg-blue-500`}>Connect</div>
             </div>
         </section>
     );
