1943e41ba278d7b18c602d5c3e5f6fd469da257d
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-08-13 13:34:00 -0700

    Decreased the space below the title so the section content is closer.

--- uoft_vac/app/components/SectionTitle.tsx
+++ uoft_vac/app/components/SectionTitle.tsx
@@ -4,7 +4,7 @@ interface SectionTitleProps {
 
 export default function SectionTitle({ children }: SectionTitleProps) {
     return (
-        <h2 className="text-8xl font-bold mb-12 text-center">
+        <h2 className="text-8xl font-bold mb-10 text-center">
             {children}
         </h2>
     );
