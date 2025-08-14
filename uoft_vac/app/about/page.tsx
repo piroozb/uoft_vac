bf44f188cd2d21d60e29e7b8bed49698bbacbd68
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-08-13 19:57:28 -0700

    Added What do we do? section.

--- uoft_vac/app/about/page.tsx
+++ uoft_vac/app/about/page.tsx
@@ -1,10 +1,12 @@
 import OurMission from "../components/about/OurMission";
+import WhatDoWeDo from "../components/about/WhatDoWeDo";
 import FunFacts from "../components/about/FunFacts";
 
 export default function About() {
   return (
     <>
       <OurMission />
+      <WhatDoWeDo />
       <FunFacts />
     </>
   )
