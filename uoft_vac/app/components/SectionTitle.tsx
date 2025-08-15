419b0db0a2552f74dc9a3857f9243f3cbd1591b1
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-08-14 17:45:55 -0700

    Added size and padding as optional arguments so we can have different titles.

--- uoft_vac/app/components/SectionTitle.tsx
+++ uoft_vac/app/components/SectionTitle.tsx
@@ -1,10 +1,12 @@
 interface SectionTitleProps {
     children: React.ReactNode;
+    size?: string;
+    padding?: string;
 }
 
-export default function SectionTitle({ children }: SectionTitleProps) {
+export default function SectionTitle({ children, size = "text-8xl", padding = "mb-10" }: SectionTitleProps) {
     return (
-        <h2 className="text-8xl font-bold mb-10 text-center">
+        <h2 className={`${size} font-bold ${padding} text-center`}>
             {children}
         </h2>
     );
