fbc92ff1d5a771af8e854dc06cdf603f56793a6e
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-08-19 14:00:18 -0700

    Made purple links better. Added a frodo placeholder but it's in the background. Text needs to wrap around it.

--- uoft_vac/app/join-us/JoinUs.tsx
+++ uoft_vac/app/join-us/JoinUs.tsx
@@ -4,7 +4,8 @@ import {
     membershipFormLink,
     discordLink,
     instagramLink,
-    textLinkPurple,
+    JoinLinkDarkPurple,
+    JoinLinkHoverPurple,
     subsectionSize,
     subsectionMargin,
 } from "../common/Constants";
@@ -13,15 +14,18 @@ import TextLink from "../common/TextLink";
 
 export default function JoinUs() {
     return (
+        // Subsection container
         <div
-            className="w-full h-[750px] rounded-md shadow-lg flex flex-col items-center justify-start p-6 overflow-y-auto"
+            className="w-full h-[750px] rounded-md shadow-lg flex flex-col items-center justify-start p-6 overflow-y-auto relative"
             style={{ backgroundColor: "rgb(255,165,0)" }}
         >
+            {/* Title */}
             <SectionTitle size={subsectionSize} margin={subsectionMargin}>
                 Join us today for free!
             </SectionTitle>
 
-            <div className="max-w-3xl text-[1.5rem] leading-relaxed">
+            {/* Contents */}
+            <div className="max-w-3xl text-[1.5rem] leading-relaxed relative z-10">
                 <ol className="list-decimal list-inside space-y-6">
                     <li>
                         Fill out the{" "}
@@ -29,7 +33,8 @@ export default function JoinUs() {
                         <TextLink
                             text="here"
                             href={membershipFormLink}
-                            style={{ color: textLinkPurple }}
+                            style={{ color: JoinLinkDarkPurple }}
+                            hoverStyle={{ color: JoinLinkHoverPurple }}
                         />.
                     </li>
 
@@ -38,7 +43,8 @@ export default function JoinUs() {
                         <TextLink
                             text="here"
                             href={discordLink}
-                            style={{ color: textLinkPurple }}
+                            style={{ color: JoinLinkDarkPurple }}
+                            hoverStyle={{ color: JoinLinkHoverPurple }}
                         />
                         . Since we manually verify members, please wait 1–2 days
                         to have full access to the server. Until then, you can
@@ -50,7 +56,8 @@ export default function JoinUs() {
                         <TextLink
                             text="here"
                             href={instagramLink}
-                            style={{ color: textLinkPurple }}
+                            style={{ color: JoinLinkDarkPurple }}
+                            hoverStyle={{ color: JoinLinkHoverPurple }}
                         />{" "}
                         for more!
                     </li>
@@ -60,9 +67,18 @@ export default function JoinUs() {
                     Watch out for our{" "}
                     <strong>event announcements and club updates</strong> on{" "}
                     <em>Discord</em> and <em>Instagram</em>! You are free to
-                    attend the events as you please :)
+                    attend the events as you please : )
                 </p>
             </div>
+
+            {/* Frodo */}
+            <div
+                className="bg-green-600 w-60 h-75 rounded-md absolute"
+                style={{
+                    bottom: 0,
+                    left: 20,
+                }}
+            />
         </div>
     );
 }
