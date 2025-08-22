76355bda08acc55a496acd2d4b2d257446916546
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-08-22 11:59:25 -0700

    Worked on Contact Us section. Will separate these two into their own pages but haven't got an idea on the layout yet.

--- uoft_vac/app/join-us/JoinUs.tsx
+++ uoft_vac/app/join-us/JoinUs.tsx
@@ -4,8 +4,8 @@ import {
     membershipFormLink,
     discordLink,
     instagramLink,
-    JoinLinkDarkPurple,
-    JoinLinkHoverPurple,
+    textLinkDarkPurple,
+    textLinkHoverPurple,
     subsectionSize,
     subsectionMargin,
 } from "../common/Constants";
@@ -16,8 +16,8 @@ export default function JoinUs() {
     return (
         // Subsection container
         <div
-            className="w-full h-[750px] rounded-md shadow-lg flex flex-col items-center justify-start p-6 overflow-y-auto relative"
-            style={{ backgroundColor: "rgb(255,165,0)" }}
+            className="w-full h-[750px] rounded-md shadow-lg flex flex-col items-center justify-start p-5 overflow-y-auto relative"
+            style={{ backgroundColor: "rgb(255, 165, 0)" }}
         >
             {/* Title */}
             <SectionTitle size={subsectionSize} margin={subsectionMargin}>
@@ -26,15 +26,15 @@ export default function JoinUs() {
 
             {/* Contents */}
             <div className="max-w-3xl text-[1.5rem] leading-relaxed relative z-10">
-                <ol className="list-decimal list-inside space-y-6">
+                <ol className="list-decimal list-inside space-y-5">
                     <li>
                         Fill out the{" "}
                         <strong>member verification form</strong> linked{" "}
                         <TextLink
                             text="here"
                             href={membershipFormLink}
-                            style={{ color: JoinLinkDarkPurple }}
-                            hoverStyle={{ color: JoinLinkHoverPurple }}
+                            style={{ color: textLinkDarkPurple }}
+                            hoverStyle={{ color: textLinkHoverPurple }}
                         />.
                     </li>
 
@@ -43,8 +43,8 @@ export default function JoinUs() {
                         <TextLink
                             text="here"
                             href={discordLink}
-                            style={{ color: JoinLinkDarkPurple }}
-                            hoverStyle={{ color: JoinLinkHoverPurple }}
+                            style={{ color: textLinkDarkPurple }}
+                            hoverStyle={{ color: textLinkHoverPurple }}
                         />
                         . Since we manually verify members, please wait 1–2 days
                         to have full access to the server. Until then, you can
@@ -56,22 +56,25 @@ export default function JoinUs() {
                         <TextLink
                             text="here"
                             href={instagramLink}
-                            style={{ color: JoinLinkDarkPurple }}
-                            hoverStyle={{ color: JoinLinkHoverPurple }}
+                            style={{ color: textLinkDarkPurple }}
+                            hoverStyle={{ color: textLinkHoverPurple }}
                         />{" "}
                         for more!
                     </li>
                 </ol>
 
+                {/* Wrapped text */}
                 <p className="mt-8">
-                    Watch out for our{" "}
-                    <strong>event announcements and club updates</strong> on{" "}
-                    <em>Discord</em> and <em>Instagram</em>! You are free to
-                    attend the events as you please : )
+                    Watch out for our <strong>event announcements and club</strong>
+                    <span className="pl-65 inline-block">
+                        <strong>updates</strong> on <em>Discord</em> and <em>Instagram!</em>
+                        <br />
+                        You are free to attend the events as you please : )
+                    </span>
                 </p>
             </div>
 
-            {/* Frodo */}
+            {/* Frodo placeholder */}
             <div
                 className="bg-green-600 w-60 h-75 rounded-md absolute"
                 style={{
