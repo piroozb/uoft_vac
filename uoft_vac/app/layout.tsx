ea157fb568df193708782e3266a4b49e179cb71a
piroozb <pirooz321@gmail.com>
2025-11-05 15:08:09 -0500

    fixed overflow behaviour, changed some spacings to fit the page, updated office hours video

--- uoft_vac/app/layout.tsx
+++ uoft_vac/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Fredoka } from "next/font/google";
 
-import { BG_LIGHT_GREEN } from "./common/Constants";
+import { BG_LIGHT_GREEN, BARS_EDGES_DARK_GREEN, BARS_CENTRE_GREEN } from "./common/Constants";
 
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
@@ -30,15 +30,46 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
+      
+      <body className="antialiased relative overflow-x-hidden">
+        {/* --- Top overscroll background (Navbar color) --- */}
+        <div
+          className="fixed top-0 left-0 w-full h-[40vh] -z-10"
+          style={{
+            background: `linear-gradient(
+              to right,
+              ${BARS_EDGES_DARK_GREEN} 0%,
+              ${BARS_CENTRE_GREEN} 33.3%,
+              ${BARS_CENTRE_GREEN} 66.6%,
+              ${BARS_EDGES_DARK_GREEN} 100%
+            )`,
+          }}
+        />
+
+      {/* --- Bottom overscroll background (Footer color) --- */}
+        <div
+          className="fixed bottom-0 left-0 w-full h-[40vh] -z-10"
+          style={{
+            background: `linear-gradient(
+              to right,
+              ${BARS_EDGES_DARK_GREEN} 0%,
+              ${BARS_CENTRE_GREEN} 33.3%,
+              ${BARS_CENTRE_GREEN} 66.6%,
+              ${BARS_EDGES_DARK_GREEN} 100%
+            )`,
+          }}
+        />
+
       {/* Light green bg */}
-      <body
-        className="antialiased"
+      <div
+        className="antialiased main-content"
         style={{ backgroundColor: BG_LIGHT_GREEN }}
       >
         <Navbar />
         {children}
         <Footer />
         <SideDecor />
+      </div>
       </body>
     </html>
   );
