46623b749b9b3b91d51d0abc95710075b60ea903
piroozb <pirooz321@gmail.com>
2025-11-09 20:49:47 -0500

    added relative paths

--- uoft_vac/next.config.ts
+++ uoft_vac/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "/uoft_vac/",
   images: {
     unoptimized: true,
   }
