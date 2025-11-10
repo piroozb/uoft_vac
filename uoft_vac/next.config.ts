9f1e20151c1bd28fff2d1f87b65c2db78f4f7252
piroozb <pirooz321@gmail.com>
2025-11-09 19:00:26 -0500

    changing config

--- uoft_vac/next.config.ts
+++ uoft_vac/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: isProd ? "/uoft_vac" : "",
+  assetPrefix: isProd ? "/uoft_vac" : "",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
