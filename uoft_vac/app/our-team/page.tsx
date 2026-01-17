6b8f811e51c8440e440249b58cdf37d605fdc4f7
Sunny Lin <iluvsunshinezduqz@gmail.com>
2026-01-17 18:07:08 -0500

    Wtf

--- uoft_vac/app/our-team/page.tsx
+++ uoft_vac/app/our-team/page.tsx
@@ -1,20 +1,6 @@
 import SectionTitle from "../common/SectionTitle";
-// import ExecEntryList from "./ExecEntryList";
 import ExecEntryGrid from "./ExecEntryGrid";
-import {
-    SMALL_TITLE_SIZE,
-    SMALL_TITLE_MARGIN,
-} from "../common/Constants";
-
-// Order:
-const team = {
-    Admin: ["Sy", "Allison", "Jackie"],
-    Events: ["Thomas", "Joyce", "Valentine"],
-    Graphics: ["BiewBiew", "Angelia", "Christina", "Rachel"],
-    Marketing: ["Jae", "Nabneel", "Emilio", "Kelvin"],
-    Webmaster: ["Pirooz", "Sunny"],
-    "Office Manager": ["Lydia", "Alex"],
-};
+import ExecEntryList from "./ExecEntryList";
 
 // export default function OurTeam() {
 //     return (
@@ -50,38 +36,75 @@ const team = {
 //     );
 // }
 
-// Explicit mapping for number of entries in a row from the array length to the tailwind version.
-const gridCols: Record<number, string> = {
+// Grid format entries order (for desktop view)
+const ENTRY_ORDER_GRID = {
+    "Admin": ["Allison", "Sy", "Jackie"],
+    "Events": ["Thomas", "Joyce", "Valentine"],
+    "Graphics": ["BiewBiew", "Angelia", "Christina", "Rachel"],
+    "Marketing": ["Jae", "Nabneel", "Emilio", "Kelvin"],
+    "Webmaster": ["Pirooz", "Sunny"],
+    "Office Manager": ["Lydia", "Alex"],
+};
+
+// Difference: switch Sy and Allison
+
+// List format entries order (for mobile view)
+const ENTRY_ORDER_LIST = {
+    "Admin": ["Sy", "Allison", "Jackie"],
+    "Events": ["Thomas", "Joyce", "Valentine"],
+    "Graphics": ["BiewBiew", "Angelia", "Christina", "Rachel"],
+    "Marketing": ["Jae", "Nabneel", "Emilio", "Kelvin"],
+    "Webmaster": ["Pirooz", "Sunny"],
+    "Office Manager": ["Lydia", "Alex"],
+};
+
+// Explicit mapping from the number of entries in a category to the tailwind equivalent (for grid format)
+const COLS_MAP: Record<number, string> = {
     1: "grid-cols-1",
     2: "grid-cols-2",
     3: "grid-cols-3",
-    4: "grid-cols-4 gap-75",
-    5: "grid-cols-5",
-    6: "grid-cols-6",
+    4: "grid-cols-4",
 };
 
 export default function OurTeam() {
     return (
-        <div className="max-w-6xl mx-auto mt-10">
-
-            {/* Title */}
+        <div className="my-5 mx-auto">
+            {/* Page title */}
             <SectionTitle>Meet the Team!</SectionTitle>
 
-            {/* Exec categories */}
             <div>
-                {Object.entries(team).map(([category, members]) => (
-                    <div key={category}>
-                        <SectionTitle size={SMALL_TITLE_SIZE} margin={SMALL_TITLE_MARGIN}>
-                            {category}
-                        </SectionTitle>
-                            <div className={`grid grid-cols-2 ${SMALL_TITLE_MARGIN}`}>
+                {Object.entries(ENTRY_ORDER_GRID).map(([category, members]) => {
+                    const cols = Math.min(members.length, 4);
+
+                    return (
+                        <div key={category}>
+
+                            {/* Category title */}
+                            <div className="my-5">
+                                <SectionTitle size="60px">
+                                    {category}
+                                </SectionTitle>
+                            </div>
+
+                            {/* Entries */}
+                            <div
+                                className={
+                                    `mx-3 gap-5 grid
+                                    grid-cols-1
+                                    ${cols >= 2 ? 'sm:grid-cols-2' : ''}
+                                    ${cols >= 3 ? 'lg:grid-cols-3' : ''}
+                                    ${cols >= 4 ? 'xl:grid-cols-4' : ''}`}
+                            >
                                 {members.map((name) => (
-                                    <ExecEntryGrid key={name} entryKey={name} />
+                                    <ExecEntryGrid
+                                        key={name}
+                                        entryKey={name}
+                                    />
                                 ))}
                             </div>
-                        
-                    </div>
-                ))}
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
