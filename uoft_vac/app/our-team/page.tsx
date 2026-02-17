30940211777892dc39b96002944fd7bb7ef66f8d
Sunny Lin <iluvsunshinezduqz@gmail.com>
2026-02-16 19:23:11 -0500

    Decoupled the exec categories from each other. There is now one EXEC_CATEGORY_SPECS containing all info and specs.

--- uoft_vac/app/our-team/page.tsx
+++ uoft_vac/app/our-team/page.tsx
@@ -2,94 +2,107 @@ import SectionTitle from "../common/SectionTitle";
 import ExecEntryGrid from "./ExecEntryGrid";
 import ExecEntryList from "./ExecEntryList";
 
-// Grid format entries order (for desktop view)
-const ENTRY_ORDER_GRID = {
-    "Admin": ["Allison", "Sy", "Jackie"],
-    "Events": ["Thomas", "Joyce", "Valentine"],
-    "Graphics": ["BiewBiew", "Angelia", "Christina"],
-    "Marketing": ["Jae", "Nabneel", "Emilio", "Kelvin"],
-    "Webmaster": ["Pirooz", "Sunny"],
-    "Office Manager": ["Lydia", "Alex"],
-};
+const CATEGORY_CONTAINER_CLASSNAME_COMMON = "mt-20";
+const CATEGORY_TITLE_HEIGHT_MAX = "10cqw";
+const CATEGORY_TITLE_HEIGHT_COMMON = `min(7rem,${CATEGORY_TITLE_HEIGHT_MAX})`;
 
-// Difference: switch Sy and Allison
+// Exec category specs:
+const EXEC_CATEGORIES_SPECS: Record<string, {
+    members: string[];
+    members_mobile?: string[];
+    titleSRC?: string;
+    titleHeight?: string;
+    containerClassName?: string;
+} >= {
 
-// List format entries order (for mobile view)
-const ENTRY_ORDER_LIST = {
-    "Admin": ["Sy", "Allison", "Jackie"],
-    "Events": ["Thomas", "Joyce", "Valentine"],
-    "Graphics": ["BiewBiew", "Angelia", "Christina"],
-    "Marketing": ["Jae", "Nabneel", "Emilio", "Kelvin"],
-    "Webmaster": ["Pirooz", "Sunny"],
-    "Office Manager": ["Lydia", "Alex"],
-};
+    "Admin": {
+        members: ["Allison", "Sy", "Jackie"],
+        members_mobile: ["Sy", "Allison", "Jackie"],
+    },
 
-const CATEGORY_TITLE_GRAPHICS: Record<string, string> = {
-    "Admin": "",
-    "Events": "",
-    "Graphics": "graphics",
-    "Marketing": "marketing",
-    "Webmaster": "webmaster",
-    "Office Manager": "office-manager",
+    "Events": {
+        members: ["Thomas", "Joyce", "Valentine"],
+        containerClassName: CATEGORY_CONTAINER_CLASSNAME_COMMON,
+    },
+
+    "Graphics": {
+        members: ["BiewBiew", "Angelia", "Christina"],
+        titleSRC: "graphics",
+        containerClassName: CATEGORY_CONTAINER_CLASSNAME_COMMON,
+        titleHeight: CATEGORY_TITLE_HEIGHT_COMMON,
+    },
+
+    "Marketing": {
+        members: ["Jae", "Nabneel", "Emilio", "Kelvin"],
+        titleSRC: "marketing",
+        containerClassName: CATEGORY_CONTAINER_CLASSNAME_COMMON,
+        titleHeight: CATEGORY_TITLE_HEIGHT_COMMON,
+    },
+
+    "Webmaster": {
+        members: ["Pirooz", "Sunny"],
+        titleSRC: "webmaster",
+        containerClassName: CATEGORY_CONTAINER_CLASSNAME_COMMON,
+        titleHeight: `min(6rem,${CATEGORY_TITLE_HEIGHT_MAX})`,
+    },
+
+    "Office Manager": {
+        members: ["Lydia", "Alex"],
+        titleSRC: "office-manager",
+        containerClassName: CATEGORY_CONTAINER_CLASSNAME_COMMON,
+        titleHeight: CATEGORY_TITLE_HEIGHT_COMMON,
+    },
 };
 
 // Explicit mapping from the number of entries in a category to the tailwind equivalent (for grid format)
 const COLS_MAP: Record<number, string> = {
-    1: "grid-cols-1",
-    2: "grid-cols-2",
-    3: "grid-cols-3",
-    4: "grid-cols-4",
-};
+        1: "grid-cols-1",
+        2: "grid-cols-2",
+        3: "grid-cols-3",
+        4: "grid-cols-4",
+    };
 
 export default function OurTeam() {
     return (
         <div className="mb-15">
+            {Object.entries(EXEC_CATEGORIES_SPECS).map( ([
+                category, {
+                    members,
+                    titleSRC,
+                    containerClassName,
+                    titleHeight,
+                }, ]) => {
 
-            {/* Page title */}
-            <SectionTitle>Meet the Team!</SectionTitle>
-
-            <div>
-                {Object.entries(ENTRY_ORDER_GRID).map(([category, members]) => {
                     const cols = Math.min(members.length, 4);
 
                     return (
-                        <div key={category}>
-
-                            {/* Category title */}
-                            <div className="mt-15">
-                                <SectionTitle
-                                    src={
-                                        CATEGORY_TITLE_GRAPHICS[category]
-                                        ? `/title-${CATEGORY_TITLE_GRAPHICS[category]}.png`
-                                        : undefined
-                                    }
-                                    height={
-                                        CATEGORY_TITLE_GRAPHICS[category]
-                                        ? "min(5rem,10cqw)"
-                                        : "4rem"
-                                    }
-                                >
-                                    {category}
-                                </SectionTitle>
-                            </div>
-
-                            {/* Entries */}
-                            <div
-                                className={`mt-5 mx-3 gap-10 grid ${cols === 4
-                                        ? "grid-cols-2 min-[1600px]:grid-cols-4"
-                                        : COLS_MAP[cols]}`}
-                            >
-                                {members.map((name) => (
-                                    <ExecEntryGrid
-                                        key={name}
-                                        entryKey={name}
-                                    />
-                                ))}
-                            </div>
+                        <div className={containerClassName}>
+                        
+                        {/* Category title */}
+                        <SectionTitle
+                            src={titleSRC ? `/title-${titleSRC}.png` : undefined}
+                            height={titleHeight}
+                        >
+                            {category}
+                        </SectionTitle>
+            
+                        {/* Entries */}
+                        <div
+                            className={`mt-5 mx-3 gap-10 grid ${cols === 4
+                                ? "grid-cols-2 min-[1600px]:grid-cols-4"
+                                : COLS_MAP[cols]
+                            }`}
+                        >
+                            {members.map((name) => (
+                                <ExecEntryGrid
+                                    key={name}
+                                    entryKey={name}
+                                />
+                            ))}
                         </div>
-                    );
-                })}
-            </div>
+                    </div>
+                )}
+            )}
         </div>
     );
 }
