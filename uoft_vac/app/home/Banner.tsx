42d8215af98aa9633594ceeba0a0460522a2b0b8
piroozb <pirooz321@gmail.com>
2026-02-26 16:49:08 -0500

    added new banner

--- uoft_vac/app/home/Banner.tsx
+++ uoft_vac/app/home/Banner.tsx
@@ -11,7 +11,7 @@ export default function Banner() {
     return (
         <div className="w-screen flex justify-center relative z-10">
             <Image
-                src="/banner-draft.jpg"
+                src="/banner-final.png"
                 alt="Banner"
                 className="w-full object-contain"
                 style={{ transform: `translateX(-${SIDE_DECOR_WIDTH * (1 - offset)}px)` }}
